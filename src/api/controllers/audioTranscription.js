610de4aa7d106165ef12cf955161f3be28e58fff
Rodrigo MP <rdenubila@gmail.com>
2020-12-11 11:25:25 -0300

    alteração local

--- src/api/controllers/audioTranscription.js
+++ src/api/controllers/audioTranscription.js
@@ -2,34 +2,60 @@
 module.exports = (app) => {
 
   const controller = {};
+
+  controller.getTranscriptions = (req, res) => {
+
   var JDBC = require('jdbc');
   var jinst = require('jdbc/lib/jinst');
 
   if (!jinst.isJvmCreated()) {
     jinst.addOption('-Xrs')
     jinst.setupClasspath([
-      './src/libs/phoenix-5.0.0.3.1.5.0-152-client.jar'
+      '/var/www/html/painelbi/software_anotacao_api/src/libs/phoenix-5.0.0.3.1.5.0-152-client.jar'
     ])
   }
 
-
-  controller.getTranscriptions = (req, res) => {
-
     var config = {
       drivername: 'org.apache.phoenix.jdbc.PhoenixDriver',
-      url: 'jdbc:phoenix:localhost:2181:/hbase-unsecure',
+      url: 'jdbc:phoenix:hadoopmn-gsi-prod01.mpmg.mp.br,hadoopmn-gsi-prod02.mpmg.mp.br,hadoopmn-gsi-prod01.mpmg.mp.br:2181:/hbase-unsecure',
       user: 'ufmg.rdenubila',
       password: 'Rdg085055147',
       maxpoolsize: 100
     }
 
-    var hsqldb = new JDBC(config);
+    var db = new JDBC(config);
 
-    hsqldb.initialize(function(err) {
+    db.initialize(function(err) {
       if (err) {
         return res.status(400).json(err);
       }
-      return res.status(200).json("FUNCIONOU");
+
+      db.reserve(function (err, connObj) {
+	if (connObj) {
+           console.log("Using JDBC connection: " + connObj.conn);
+           var conn = connObj.conn;
+
+	conn.createStatement(function(err, statement) {
+		if (err) {
+			return res.status(400).json(err);
+		} else {
+			statement.executeQuery("select DISTINCT('TABLE_NAME') from SYSTEM.CATALOG;", function(err, resultset) {
+			if (err) {
+				return res.status(400).json(err);
+			} else {
+				// Convert the result set to an object array.
+				resultset.toObjArray(function(err, results) {
+					return res.status(200).json(results);
+				});
+			}
+		}
+	})
+
+	} else {
+	   return res.status(400).json(err);
+        }
+      });
+
     });
 
     /*
