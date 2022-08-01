8cc944119dc7c91543a556e012516b0181ccf1c3
Thiago Correia <thiagocorreiamonteiro@gmail.com>
2022-08-01 09:46:21 -0300

    Changing server port and adding image to case types

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,23 @@
-import express from 'express';
-require('dotenv').config();
+import express from "express";
+require("dotenv").config();
 
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
-const routes = require('./routes/index');
-const fileUpload = require('express-fileupload');
-
+const port = 3000;
+const routes = require("./routes/index");
+const fileUpload = require("express-fileupload");
 
 app.use(express.json());
 app.use(cors());
 app.use(fileUpload());
 app.use((req, res, next) => {
-  req.body.uploadPath = require('path').resolve('./') + '/public/files/';
-  req.body.publicPath = require('path').resolve('./') + '/public/';
+  req.body.uploadPath = require("path").resolve("./") + "/public/files/";
+  req.body.publicPath = require("path").resolve("./") + "/public/";
   next();
-})
+});
 
-app.get('/', (req, res) => res.send("Bem vindo"));
-app.use('/api', routes);
-
+app.get("/", (req, res) => res.send("Bem vindo"));
+app.use("/api", routes);
 
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
--- src/models/project.ts
+++ src/models/project.ts
@@ -2,82 +2,88 @@ import { DbConnector } from "../services/dbConnector";
 import ProjectUser from "./project_user";
 import User from "./user";
 
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 
-const Project = DbConnector.sequelize().define('projects', {
+const Project = DbConnector.sequelize().define(
+  "projects",
+  {
     // Model attributes are defined here
     id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: Sequelize.UUIDV4
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
     },
     user_id: {
-        type: DataTypes.UUID,
-        allowNull: true,
-        references: {
-            model: User,
-            key: 'id'
-        }
+      type: DataTypes.UUID,
+      allowNull: true,
+      references: {
+        model: User,
+        key: "id",
+      },
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     description: {
-        type: DataTypes.TEXT,
-        allowNull: true
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     has_audio_transcription: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    has_image: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     has_entities_detection: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     has_paraphrases: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     has_classification: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     classification_multiple_labels: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     classification_users_per_segment: {
-        type: DataTypes.INTEGER,
-        defaultValue: 1
+      type: DataTypes.INTEGER,
+      defaultValue: 1,
     },
     audio_allow_download: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     entities_allow_relationship: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     color: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     icon: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     open: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true
-    }
-}, {
-  // Other model options go here
-  paranoid: true
-});
-
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  },
+  {
+    // Other model options go here
+    paranoid: true,
+  }
+);
 
 (async () => {
-    await Project.sync({alter: true});
+  await Project.sync({ alter: true });
 })();
-
 
 export default Project;
